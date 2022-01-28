fba24818d4d20fc4a619a1887468e04020ae9eed
ToodlyDave <alapatdavid@gmail.com>
2022-01-28 23:31:33 +0530

    ADD entity and dto layers

---
+++ src/main/java/com/workshop/WorkshopAddressBook/dto/AddressDTO.java
@@ -0,0 +1,10 @@
+package com.workshop.WorkshopAddressBook.dto;
+
+import javax.persistence.Entity;
+
+@Entity
+public class AddressDTO {
+
+	String name;
+	String address;
+}
---
+++ src/main/java/com/workshop/WorkshopAddressBook/entities/Address.java
@@ -0,0 +1,11 @@
+package com.workshop.WorkshopAddressBook.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Address {
+
+	long id;
+	String name;
+	String address;
+}
